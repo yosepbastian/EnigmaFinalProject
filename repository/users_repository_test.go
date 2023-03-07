1428dcdf0113186c38fa5f6be6b85c2c431d4386
musasianturi <musasianturi00@gmail.com>
2023-03-07 22:35:19 +0700

    unit test

---
+++ repository/users_repository_test.go
@@ -0,0 +1,76 @@
+package repository
+
+import (
+	
+	"kel1-stockbite-projects/models"
+	"log"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+var dummyUser =  models.Users{
+	Id:       "24912488124",
+	Name:     "John Doe",
+	Email:    "john.doe@example.com",
+	Password: "password123",
+	Balance:  100.0,
+}
+
+type UsersRepositoryTestSuite struct {
+	suite.Suite
+	mockResource *sqlx.DB
+	mock         sqlmock.Sqlmock
+}
+
+func (suite *UsersRepositoryTestSuite) SetupTest() {
+	mockdb, mock, err := sqlmock.New()
+	if err != nil {
+		log.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	sqlxDB := sqlx.NewDb(mockdb, "sqlmock")
+	suite.mockResource = sqlxDB
+	suite.mock = mock
+}
+
+func (suite *UsersRepositoryTestSuite) TearDownTest() {
+	suite.mockResource.Close()
+}
+
+func (suite *UsersRepositoryTestSuite) GetUserBalance_Success() {
+	// balance := dummyUser.Balance
+
+	// suite.mock
+
+}
+
+func (suite *UsersRepositoryTestSuite) TestUsersRepository_Insert() {
+	// Prepare the user data for insertion.
+	user := models.Users{
+		Id:       "24912488124",
+		Name:     "John Doe",
+		Email:    "john.doe@example.com",
+		Password: "password123",
+		Balance:  100.0,
+	}
+
+	// Configure mock to expect the insert query.
+	suite.mock.ExpectExec("^INSERT INTO users").
+		WithArgs(user.Id, user.Name, user.Email, user.Password, user.Balance).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	// Call the insert method.
+	repo := NewUsersRepository(suite.mockResource)
+	err := repo.Insert(&user)
+	assert.Nil(suite.T(), err)
+}
+func TestUsersRepository_GetById(t *testing.T) {
+	
+}
+
+func TestStudentRepositoryTestSuite(t *testing.T) {
+	suite.Run(t, new(UsersRepositoryTestSuite))
+}
